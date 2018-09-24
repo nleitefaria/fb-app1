79a4f5a52ce07fc134d068a3d7b21f64a5bebd7c
nleitefaria <nleitefaria@gmail.com>
2018-09-24 15:24:03 +0100

    Updated aggregates

--- src/app/components/aggregates/aggregates.component.ts
+++ src/app/components/aggregates/aggregates.component.ts
@@ -1,102 +1,117 @@
 import { Component, OnInit } from '@angular/core';
 import { AggregatesService } from '../../services/aggregates.service';
 import { Country } from '../../model/country';
+import { IncomeLevels } from '../../model/income-levels';
 
-@Component({
-  selector: 'app-aggregates',
-  templateUrl: './aggregates.component.html',
-  styleUrls: ['./aggregates.component.css'],
-  providers: [AggregatesService]
-})
+@Component( {
+    selector: 'app-aggregates',
+    templateUrl: './aggregates.component.html',
+    styleUrls: ['./aggregates.component.css'],
+    providers: [AggregatesService]
+} )
 export class AggregatesComponent implements OnInit {
 
     indicators: any[];
     p: number = 1;
     total: number;
     loading: boolean;
-    location: string = "Aggregates"; 
+    location: string = "Aggregates";
+    cId: string;
+    ilcId: string;
 
     countries: Country[] = [
-       {"id": "ARB", "name": "Arab World"},
-       {"id": "BRA", "name": "Brazil"},
-       {"id": "CAN", "name": "Canada"}
-       ];
-
+        { "id": "ARB", "name": "Arab World" },
+        { "id": "BRA", "name": "Brazil" },
+        { "id": "CAN", "name": "Canada" }
+    ];
     selectedCountriesFilterData: Country = this.countries[0];
-  
-    constructor(private httpService : AggregatesService) 
-    {   
-    } 
-  
-    ngOnInit() 
-    {
-      this.init();  
-    }
-    
-    init()
-    {  
-      this.getPage(1);
-      this.getAggregatesForPageAndCountry(1, 'ARB');
-    }
-  
-    getPage(page: number)
-    {
-      this.loading = true;  
-        this.httpService.getAggregates('br', 'NY.GDP.MKTP.CD', page).subscribe(
-      response =>{
-        if(response.error) { 
-          alert('Server Error');
-        } else {                        
-          this.p = page;                                
-          this.total = response[0].total;
-          this.indicators = response[1];       
-          this.loading = false;                   
-        }
-      },
-      error =>{
-        alert('Server error');
-      }
-    );
-    }
-    
-    
-    getAggregatesForPageAndCountry(page: number, country: string)
-    {
-        
-        this.loading = true;  
-        this.httpService.getAggregates(country, 'NY.GDP.MKTP.CD', page).subscribe(
-      response =>{
-        if(response.error) { 
-          alert('Server Error');
-        } else {                        
-          this.p = page;                                
-          this.total = response[0].total;
-          this.indicators = response[1];       
-          this.loading = false;                   
-        }
-      },
-      error =>{
-        alert('Server error');
-      }
-    );
-        
-        
-        
-    }
-    
-    
-    
-    onSelectCountriesFilter(countryId) {
-        console.log('ICI');
-        this.selectedCountriesFilterData = null;
-        for (var i = 0; i < this.countries.length; i++) {
-          if (this.countries[i].id == countryId) {
-            this.selectedCountriesFilterData = this.countries[i];
-          }
-        }
-        
-           this.getAggregatesForPageAndCountry(this.p, countryId);
-           
-      }
 
+    incomeLevelsData: IncomeLevels[] = [
+        { "id": "NY.GDP.MKTP.CD", "name": "NY.GDP.MKTP.CD" },
+        { "id": "TODO", "name": "TODO" }
+    ];
+    selectedIncomeLevelsFilterData: IncomeLevels = this.incomeLevelsData[0];
+
+    constructor( private httpService: AggregatesService ) {
+    }
+
+    ngOnInit() {
+        this.init();
+    }
+
+    init() {
+        this.cId = 'ARB';
+        this.ilcId = 'NY.GDP.MKTP.CD';
+        this.getAggregatesForPageAndCountryAndIncomeLevels( 1, 'ARB', 'NY.GDP.MKTP.CD' );
+    }
+
+    getPage( page: number ) {
+        this.loading = true;
+        this.httpService.getAggregates( this.cId, this.ilcId, page ).subscribe(
+            response => {
+                if ( response.error ) {
+                    alert( 'Server Error' );
+                } else {
+                    this.p = page;
+                    this.total = response[0].total;
+                    this.indicators = response[1];
+                    this.loading = false;
+                }
+            },
+            error => {
+                alert( 'Server error' );
+            }
+        );
+    }
+
+    getAggregatesForPageAndCountryAndIncomeLevels( page: number, country: string, incomeLevels: string ) {
+        this.loading = true;
+        this.httpService.getAggregates( country, incomeLevels, page ).subscribe(
+            response => {
+                if ( response.error ) {
+                    alert( 'Server Error' );
+                } else {
+                    this.p = page;
+                    this.total = response[0].total;
+                    this.indicators = response[1];
+                    this.loading = false;
+                }
+            },
+            error => {
+                alert( 'Server error' );
+            }
+
+        );
+
+    }
+
+    onSelectCountriesFilter( countryId )
+    {      
+        this.selectedCountriesFilterData = null;
+        for ( var i = 0; i < this.countries.length; i++ ) 
+        {
+            if ( this.countries[i].id == countryId )
+            {
+                this.selectedCountriesFilterData = this.countries[i];
+            }
+        }
+
+        this.cId = countryId;
+        this.getAggregatesForPageAndCountryAndIncomeLevels( this.p, this.cId, this.ilcId );
+    }
+
+    onSelectIncomeLevelCodesFilter( incomeLevelCodesId )
+    {
+        
+        this.selectedIncomeLevelsFilterData = null;
+        for ( var i = 0; i < this.incomeLevelsData.length; i++ )
+        {
+            if ( this.incomeLevelsData[i].id == incomeLevelCodesId ) 
+            {
+                this.selectedIncomeLevelsFilterData = this.incomeLevelsData[i];
+            }
+        }
+        this.ilcId = incomeLevelCodesId;
+        this.getAggregatesForPageAndCountryAndIncomeLevels( this.p, this.cId, this.ilcId );
+    }
 }
---
+++ src/app/model/income-levels.ts
@@ -0,0 +1,5 @@
+export class IncomeLevels
+{
+    id: string;
+    name: string;
+}
